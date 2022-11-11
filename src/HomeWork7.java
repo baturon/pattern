e22e6882b234760715a2f3855ad7d810c7b472a5
baturon <vov.85@mail.ru>
2022-11-11 16:14:35 +0300

    dz

--- src/HomeWork7.java
+++ src/HomeWork7.java
@@ -3,9 +3,9 @@ import java.util.Arrays;
 public class HomeWork7 {
 
     public static void main(String[] args) {
-        int[] nums = {8,1,9,2,82,7,3,6,9,1,6};
-        System.out.println(GrassHopper.findAverage(nums));
-        System.out.println(SumOfDifferences.sumOfDifferences(nums));
+        int[] nums = {8, 1, 9, 2, 82, 7, 3, 6, 9, 1, 6};
+        System.out.println("Среднее арифметическое: " + GrassHopper.findAverage(nums));
+        System.out.println("Сумма разности элементов массива: " + SumOfDifferences.sumOfDifferences(nums));
     }
 
     //1). Find the mean (average) of a list of numbers in an array.
@@ -26,12 +26,11 @@ public class HomeWork7 {
     class SumOfDifferences {
         public static int sumOfDifferences(int[] nums) {
             Arrays.sort(nums);
-            System.out.println(Arrays.toString(nums));
             int sum = 0;
             if (nums.length == 1) {
                 return 0;
             }
-            for (int i = nums.length-1; i > 0; i--) {
+            for (int i = nums.length - 1; i > 0; i--) {
                 sum += nums[i] - nums[i - 1];
             }
             return sum;
